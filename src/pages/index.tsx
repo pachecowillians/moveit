44b32206726d764984ac506eea63c206168b54f8
Willian Pacheco <willianpacheco31@gmail.com>
2021-06-21 14:02:44 -0300

    Fix layout to fit screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,50 +12,50 @@ import { ChallengesProvider } from '../contexts/ChallengesContext'
 import styles from '../styles/pages/Home.module.css'
 
 interface HomeProps {
-  level: number;
-  currentExperience: number;
-  challengesCompleted: number;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 }
 
 export default function Home(props: HomeProps) {
-  return (
-    <ChallengesProvider
-      level={props.level}
-      currentExperience={props.currentExperience}
-      challengesCompleted={props.challengesCompleted}
-    >
-      <div className={styles.container}>
-        <Head>
-          <title>Início | move.it</title>
-        </Head>
-        <ExperienceBar />
+    return (
+        <ChallengesProvider
+            level={props.level}
+            currentExperience={props.currentExperience}
+            challengesCompleted={props.challengesCompleted}
+        >
+            <div className={styles.container}>
+                <Head>
+                    <title>Início | move.it</title>
+                </Head>
+                <ExperienceBar />
 
-        <CountdownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenges />
-              <Countdown />
+                <CountdownProvider>
+                    <section>
+                        <div>
+                            <Profile />
+                            <CompletedChallenges />
+                            <Countdown />
+                        </div>
+                        <div>
+                            <ChallengeBox />
+                        </div>
+                    </section>
+                </CountdownProvider>
             </div>
-            <div>
-              <ChallengeBox />
-            </div>
-          </section>
-        </CountdownProvider>
-      </div>
-    </ChallengesProvider>
-  )
+        </ChallengesProvider>
+    )
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
-  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+    const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
 
-  return {
-    props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted),
+    return {
+        props: {
+            level: Number(level),
+            currentExperience: Number(currentExperience),
+            challengesCompleted: Number(challengesCompleted),
+        }
     }
-  }
 }
